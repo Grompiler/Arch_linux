c200c5ba3530a08a68bbbafed5b9fa4adc4ac5da
Pierre Leblond <32479816+Pioterr@users.noreply.github.com>
2019-11-01 19:02:51 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ while getopts ":a:r:b:p:h" o; do case "${o}" in
 esac done
 
 # DEFAULTS:
-[ -z "$dotfilesrepo" ] && dotfilesrepo="https://github.com/Pioterr/dot_files.git
+[ -z "$dotfilesrepo" ] && dotfilesrepo="https://github.com/Pioterr/dot_files.git"
 [ -z "$progsfile" ] && progsfile="https://raw.githubusercontent.com/Pioterr/Arch_linux/master/progs.csv"
 [ -z "$aurhelper" ] && aurhelper="yay"
 [ -z "$repobranch" ] && repobranch="master"
